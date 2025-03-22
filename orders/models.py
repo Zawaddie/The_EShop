28059d83711210d95f5601a145c58fe2cd8d51c6
Zawaddie <zeddieburu87@gmail.com>
2025-03-22 06:33:02 -0400

    projects models created

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255,unique=True)
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255,null=True)
+    description= models.TextField(default="")
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class Orders(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product,)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"order {self.pk} by {self.user}"
